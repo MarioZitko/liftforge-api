257b0132aa4a5717a26803c7f76c75a308e4869e
Mario Žitković <106630759+MarioZitko@users.noreply.github.com>
2025-06-02 21:37:49 +0200

    19 - Seed all common exercises (#17)
    19 - Seed all common exercises

--- src/modules/exercise/dto/create-exercise.dto.ts
+++ src/modules/exercise/dto/create-exercise.dto.ts
@@ -1,15 +1,30 @@
-// src/modules/exercise/dto/create-exercise.dto.ts
-import { IsOptional, IsString, IsUrl } from 'class-validator';
+import { IsOptional, IsString, IsUrl, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateExerciseDto {
+  @ApiProperty()
   @IsString()
   name!: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsUrl()
   tutorialUrl?: string;
+
+  @ApiProperty({ required: false, type: [String] })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  primaryMuscles?: string[];
+
+  @ApiProperty({ required: false, type: [String] })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  secondaryMuscles?: string[];
 }
