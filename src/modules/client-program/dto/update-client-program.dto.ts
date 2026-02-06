ed4c382f1a715cdd90da2ff7d14eb233d6a026d2
Ante Perković <109663078+ante-perkovic@users.noreply.github.com>
2026-02-06 18:51:59 +0100

    Revert "WIP (#23)"
    This reverts commit 65dbf4f2f2c535e722e5509033e454a52caedbfa.

--- src/modules/client-program/dto/update-client-program.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateClientProgramDto } from './create-client-program.dto';
-
-export class UpdateClientProgramDto extends PartialType(CreateClientProgramDto) {}
