d4768f358f147da5946ebb344971828a1fa78124
ante-perkovic <ante.perkovic6625@gmail.com>
2026-08-26 21:51:59 +0200

    WIP

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
