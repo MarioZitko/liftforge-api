d096687b267d863de504cfd68424eeb1e0dc5205
ante-perkovic <ante.perkovic6625@gmail.com>
2026-09-03 00:26:16 +0200

    WIP

--- src/modules/client/dto/create-client.dto.ts
+++ src/modules/client/dto/create-client.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsOptional, IsString } from 'class-validator';
 
 export class CreateClientDto {
+  @ApiProperty()
   @IsString()
   userId!: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsDateString()
   dateOfBirth?: string;
--- src/modules/coach/dto/invite-client.dto.ts
+++ src/modules/coach/dto/invite-client.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class InviteClientDto {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email!: string;
